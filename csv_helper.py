5bcf5528405815d4b76d10805315ad893f591b7f
dannyyua <dannyyua@gmail.com>
2026-05-01 23:57:25 -0700

    Reuse some CSV code

--- csv_helper.py
+++ csv_helper.py
@@ -4,6 +4,19 @@ import csv
 import re
 from globals import temp_file
 
+def get_csv_rows():
+    # TODO: Yield
+    # TODO: Check if file is not empty
+    with open(temp_file, 'r', newline='') as file:
+        reader = csv.reader(file)
+
+        rows = []
+
+        for row in reader:
+            rows.append(row)
+
+        return rows
+
 # Copy the given CSV to a temp CSV file for assigning splits to
 def transform_csv(filename):
     # Trim quotations
@@ -16,15 +29,7 @@ def transform_csv(filename):
     
     shutil.copy(filename, temp_file)
 
-    rows = []
-    
-    with open(temp_file, 'r', newline='') as file:
-        reader = csv.reader(file)
-
-        # TODO: Yield
-        # TODO: Check if file is not empty
-        for row in reader:
-            rows.append(row)
+    rows = get_csv_rows()
 
     rows[0].append("Split IDs")
     for row in rows[1:]:
@@ -36,40 +41,34 @@ def transform_csv(filename):
 
 # Calculate the amounts owed by each payer based on the splits assigned to each row
 def calculate_amounts(ps_dict):
-    with open(temp_file, 'r', newline='') as file:
-        reader = csv.reader(file)
+    rows = get_csv_rows()
 
-        rows = []
+    totals_dict = {}
 
-        for row in reader:
-            rows.append(row)
+    for i in range(1, len(rows)):
+        split_id = rows[i][-1]
 
-        totals_dict = {}
+        # Ignore if split ID is 0
+        if split_id == '0':
+            continue
 
-        for i in range(1, len(rows)):
-            split_id = rows[i][-1]
+        # Try to find a currency amount in the row
+        matches = [bool(re.match(r'^-?\d+\.?\d*$', val)) for val in rows[i][:-1]]
+        if True not in matches:
+            print(f"Warning: No transaction amount found in row {i+1}. Ignoring row.")
+            continue
 
-            # Ignore if split ID is 0
-            if split_id == '0':
-                continue
+        amount_indx = matches.index(True)
+        amount = float(rows[i][amount_indx])
 
-            # Try to find a currency amount in the row
-            matches = [bool(re.match(r'^-?\d+\.?\d*$', val)) for val in rows[i][:-1]]
-            if True not in matches:
-                print(f"Warning: No transaction amount found in row {i+1}. Ignoring row.")
-                continue
+        if split_id not in ps_dict:
+            print(f"Warning: Unknown Split ID '{split_id}' found in row {i+1}. Ignoring row. Please update the Split ID if this row should be included.")
+            continue
 
-            amount_indx = matches.index(True)
-            amount = float(rows[i][amount_indx])
+        for (name, percent) in ps_dict[split_id]:
+            if name not in totals_dict:
+                totals_dict[name] = 0
 
-            if split_id not in ps_dict:
-                print(f"Warning: Unknown Split ID '{split_id}' found in row {i+1}. Ignoring row. Please update the Split ID if this row should be included.")
-                continue
-
-            for (name, percent) in ps_dict[split_id]:
-                if name not in totals_dict:
-                    totals_dict[name] = 0
-
-                totals_dict[name] += amount * (percent / 100)
-            
-        return totals_dict
+            totals_dict[name] += amount * (percent / 100)
+        
+    return totals_dict
